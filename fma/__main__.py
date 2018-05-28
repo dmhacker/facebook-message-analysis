79b52efc4179590fcbdfdd3530550dbb224c467f
David Hacker <dmhacker.cs@gmail.com>
2018-05-27 21:14:23 -0700

    Rename module name

--- fma/__main__.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import analyzer
-
-parser = argparse.ArgumentParser(description='Tool to analyze your Facebook Messenger history')
-parser.add_argument('file', help='Facebook chat messages in JSON format') 
-
-args = parser.parse_args()
-analyzer.analyze(args.file)
